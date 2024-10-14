b1baaad79d1fb28da9c2ca6c3dc1c3f5f92b0561
vadimkarnopelev <vadim7j7@gmail.com>
2024-10-14 14:03:47 +0800

    Add some methods to Connection service.

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -11,11 +11,24 @@ class Connection < ApplicationRecord
   validates :uuid, presence: true, if: -> { connected? }
   validates :uuid, uniqueness: { scope: %i[category provider] }
 
+  # @return[Hash]
   def credentials_parsed
     return {} if credentials.blank?
 
     @credentials_parsed ||= JSON.parse(credentials).deep_symbolize_keys
   rescue JSON::ParserError
     {}
+  end
+
+  # @return[Boolean]
+  def reconnect_able?
+    credentials_parsed[:refresh_token].present?
+  end
+
+  # @return[Boolean]
+  def expired?
+    return true if expired_at.nil?
+
+    Time.zone.now >= expired_at
   end
 end
